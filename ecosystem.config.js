d8fbfe9853a5d940c51f6e80ca3d7d9457415ed9
Victor <vhlp.develop@gmail.com>
2023-01-21 22:24:35 -0400

    Hotfix pastas

--- ecosystem.config.js
+++ ecosystem.config.js
@@ -1,24 +1,25 @@
+
 module.exports = {
     apps : [
     {
       name: "Admin Service",
-      script: "./src/Admin Service/app.js",
+      script: "./src/Admin_Service/app.js",
     },
     {
       name: "Auth Service",
-      script: "./src/Auth Service/app.js",
+      script: "./src/Auth_Service/app.js",
     },
     {
         name: "Crud Service",
-        script: "./src/Crud Service/app.js",
+        script: "./src/Crud_Service/app.js",
     },
     {
         name: "Entrance Service",
-        script: "./src/Entrance Service/app.js",
+        script: "./src/Entrance_Service/app.js",
     },
     {
         name: "Front Service",
-        script: "./src/Front Service/app.js",
+        script: "./src/Front_Service/app.js",
     },
     {
         name: "API GATEWAY",

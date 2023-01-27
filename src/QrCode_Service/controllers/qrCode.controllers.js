9bd432f885fadc98b46637bd57c81a5bf83f8a14
Victor <vhlp.develop@gmail.com>
2023-01-27 14:45:44 -0400

    teste

--- src/QrCode_Service/controllers/qrCode.controllers.js
+++ src/QrCode_Service/controllers/qrCode.controllers.js
@@ -465,7 +465,7 @@ module.exports = {
       
           const sellRegistry = await sellRegistryModel.create(objeto_registro);
           if (sellRegistry) {
-           
+           console.log(sellRegistry.createdAt)
             const qrcodeUpdater = await QrCodesModel.findByIdAndUpdate(
               qrcode._id,
               qrcode

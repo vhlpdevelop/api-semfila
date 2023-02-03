dd8703b109cb7e51aab737c74b238d105d2d6113
Victor <vhlp.develop@gmail.com>
2023-02-02 23:41:45 -0400

    teste 2

--- src/Entrance_Service/controllers/methods/withDrawFunction.js
+++ src/Entrance_Service/controllers/methods/withDrawFunction.js
@@ -12,38 +12,36 @@ module.exports = {
             const reqGN = await reqGNAlready;
             const cobResponse = await reqGN.get(`/v2/cob/${pedido.txid}`);
             console.log(pedido.price)
-            const pix = await reqGN.put(`/v2/pix/${cobResponse.data.pix[0].endToEndId}/devolucao/${pedido.txid}`, { valor: '0.10'})
-            if(pix.data.id){
-                if (pedido.user_email) { //Caso tenha um usuario enviar um email
-                    let escopo = "Infelizmente realizamos um reembolso inesperado. "
-                    let mensagem = "O Reembolso vindo do pedido " + pedido._id + " no valor de: R$" + valor + 
+            await reqGN.put(`/v2/pix/${cobResponse.data.pix[0].endToEndId}/devolucao/${pedido.txid}`, { valor: '0.10' })
+            if (pedido.user_email) { //Caso tenha um usuario enviar um email
+                let escopo = "Infelizmente realizamos um reembolso inesperado. "
+                let mensagem = "O Reembolso vindo do pedido " + pedido._id + " no valor de: R$" + valor +
                     ". Sentimos muito, mas o Estoque de um dos itens está zerado, por isso realizamos seu reembolso. Por SemFila."
-                    mailer.sendMail(
-                        {
-                            to: pedido.user_email,
-                            from: mailerconfig.from,
-                            template: "EmailTemplateBasic",
-                            subject: "SemFila - Reembolso efetuado.",
-                            context: { escopo, mensagem },
-                        },
-                        (err) => {
-                            if (err) {
-                                console.log(err);
-                                return { success:false, msg: err.message}
-                            }
+                mailer.sendMail(
+                    {
+                        to: pedido.user_email,
+                        from: mailerconfig.from,
+                        template: "EmailTemplateBasic",
+                        subject: "SemFila - Reembolso efetuado.",
+                        context: { escopo, mensagem },
+                    },
+                    (err) => {
+                        if (err) {
+                            console.log(err);
+                            return { success: false, msg: err.message }
                         }
-                    );
-    
-                }
+                    }
+                );
+
             }
-            
+
             return { success: true }
         } catch (e) {
             console.log(e)
-          return { success:false, msg: e.message}
+            return { success: false, msg: e.message }
         }
     },
-    async withDrawLostItem(pedido){
+    async withDrawLostItem(pedido) {
         try {
             const reqGNAlready = GNRequest({
                 clientID: process.env.GN_CLIENT_ID,
@@ -54,34 +52,31 @@ module.exports = {
             const cobResponse = await reqGN.get(`/v2/cob/${pedido.txid}`);
             //valor: pedido.price
             const pix = await reqGN.put(`/v2/pix/${cobResponse.data.pix[0].endToEndId}/devolucao/${pedido.txid}`, { valor: '0.10' })
-            if(pix.data.id){
-                if (pedido.user_email) { //Caso tenha um usuario enviar um email
-                    let escopo = "Infelizmente realizamos um reembolso inesperado. "
-                    let mensagem = "O Reembolso vindo do pedido " + pedido._id + " no valor de: R$" + valor + 
+            if (pedido.user_email) { //Caso tenha um usuario enviar um email
+                let escopo = "Infelizmente realizamos um reembolso inesperado. "
+                let mensagem = "O Reembolso vindo do pedido " + pedido._id + " no valor de: R$" + valor +
                     ". Sentimos muito, mas um item não foi encontrado, por isso realizamos seu reembolso. Por SemFila."
-                    mailer.sendMail(
-                        {
-                            to: pedido.user_email,
-                            from: mailerconfig.from,
-                            template: "EmailTemplateBasic",
-                            subject: "SemFila - Reembolso efetuado.",
-                            context: { escopo, mensagem },
-                        },
-                        (err) => {
-                            if (err) {
-                                console.log(err);
-                                return { success:false, msg: err.message}
-                            }
+                mailer.sendMail(
+                    {
+                        to: pedido.user_email,
+                        from: mailerconfig.from,
+                        template: "EmailTemplateBasic",
+                        subject: "SemFila - Reembolso efetuado.",
+                        context: { escopo, mensagem },
+                    },
+                    (err) => {
+                        if (err) {
+                            console.log(err);
+                            return { success: false, msg: err.message }
                         }
-                    );
-    
-                }
+                    }
+                );
+
             }
-            
             return { success: true }
-        } catch (e) {
-            console.log(e)
-          return { success:false, msg: e.message}
-        }
+    } catch(e) {
+        console.log(e)
+        return { success: false, msg: e.message }
     }
+}
 }

c2bd0d9c67485688c6af53f2eb3a0b2d16b6d319
Victor <vhlp.develop@gmail.com>
2023-02-07 14:31:29 -0400

    teste rembolso

--- src/Crud_Service/controllers/methods/withDrawFunction.js
+++ src/Crud_Service/controllers/methods/withDrawFunction.js
@@ -6,10 +6,10 @@ module.exports = {
                 clientID: process.env.GN_CLIENT_ID,
                 clientSecret: process.env.GN_CLIENT_SECRET,
             });
-
+            console.log(valor)
             const reqGN = await reqGNAlready;
             const cobResponse = await reqGN.get(`/v2/cob/${pedido.txid}`);
-            await reqGN.put(`/v2/pix/${cobResponse.data.pix[0].endToEndId}/devolucao/${pedido.txid}`, { valor: valor }) //ALTERAR DEPOIS
+            await reqGN.put(`/v2/pix/${cobResponse.data.pix[0].endToEndId}/devolucao/${pedido.txid}`, { valor: "0.10" }) //ALTERAR DEPOIS
         } catch (e) {
             console.log(e.message)
             return { success: false, msg: "ocorreu um erro"}

225cc9e279c61267a9ea58e13f3da810cd6d1fde
Victor <vhlp.develop@gmail.com>
2023-04-26 15:24:51 -0400

    hotfix

--- src/app.js
+++ src/app.js
@@ -63,12 +63,20 @@ app.post('/stripeWebhook', express.raw({type: 'application/json'}), (request, re
   let event;
 
   try {
-    event = stripe.webhooks.constructEvent(request.body, sig, endpointSecret);
+    event = stripe.webhooks.constructEvent(request.body, sig, process.env.STRIPE_ENDPOINT_SECRET);
   } catch (err) {
     response.status(400).send(`Webhook Error: ${err.message}`);
     return;
   }
-  console.log(`Unhandled event type ${event.type}`);
+  switch (event.type) {
+    case 'payment_intent.succeeded':
+      const paymentIntentSucceeded = event.data.object;
+      // Then define and call a function to handle the event payment_intent.succeeded
+      break;
+    // ... handle other event types
+    default:
+      console.log(`Unhandled event type ${event.type}`);
+  }
 
   // Return a 200 response to acknowledge receipt of the event
   response.send();

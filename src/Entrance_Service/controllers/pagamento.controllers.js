482a1d0025b7f229d386a6bda6d24fcde23d4818
Victor <vhlp.develop@gmail.com>
2023-05-23 15:00:54 -0400

    show errors

--- src/Entrance_Service/controllers/pagamento.controllers.js
+++ src/Entrance_Service/controllers/pagamento.controllers.js
@@ -917,7 +917,7 @@ module.exports = {
         });
       }
     } catch (e) {
-      //console.log(e);
+      console.log(e);
       return res.send({
         success: false,
         msg: "Ops, ocorreu um erro",

24bfdc4541f9bdfe6806a49cc26872452d4bb6fb
Victor <vhlp.develop@gmail.com>
2023-07-10 14:52:49 -0400

    arrumar outro de novo

--- src/QrCode_Service/controllers/qrCode.controllers.js
+++ src/QrCode_Service/controllers/qrCode.controllers.js
@@ -11,6 +11,7 @@ const itemModel = require("../../models/items.model")
 const qr = require("qr-image");
 
 
+
 const {sendQrCodeUpdates} = require("../utils/sendMessages")
 module.exports = {
   async requestWithDraw(req, res) {
@@ -668,7 +669,23 @@ module.exports = {
             }
           }
           console.log(qrcodes_to_complete)
-          
+          //REMOVER APOS CONSERTAR
+          let datatoEncrypt = {
+            _id: qrcode[0]._id,
+            store_id: qrcode[0].store_id,
+          };
+
+          let texto = Encrypter.encrypt(datatoEncrypt);
+          const code = qr.imageSync(texto, {
+            type: "png",
+            size: 10,
+          });
+
+          //console.log(code);
+          var base64data = Buffer.from(code, "binary").toString("base64");
+          //console.log(base64data);
+          qrcode[0].QrImage = base64data;
+          await QrCodesModel.findByIdAndUpdate({_id: qrcode[0]._id}, qrcode[0])
           if(type){
             return res.send({ success: true, msg: "Pedido localizado", obj: qrcode, isType:type, objectType: qrcodes_to_complete})
           }else{

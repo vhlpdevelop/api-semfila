d6ed5540808bdcee1eb3c9a167c821faac7475b0
Victor Hugo Lopes Poli <66642595+vhlpdevelop@users.noreply.github.com>
2025-01-28 16:00:34 -0400

    Update db.js

--- src/config/db.js
+++ src/config/db.js
@@ -1,7 +1,7 @@
 const mongoose = require('mongoose');
 
-const URI = 'mongodb+srv://vhlpdevelp:twater22@smartline-bd-0.rkwpqow.mongodb.net/?retryWrites=true&w=majority';
-//const URI = 'mongodb://localhost/apiextensaoLADS';
+const URI = '';
+
 
 mongoose.connect(URI, {
   useNewUrlParser: true, 
